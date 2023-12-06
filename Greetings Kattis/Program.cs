2546eabeb538bbf5d741695d62b70e4f5d1375cb
Br0deNN <theo.broden@gmail.com>
2023-12-06 20:52:49 +0100

    Add project files.

---
+++ Greetings Kattis/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace hey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = new StringBuilder();
+            string input = Console.ReadLine();
+
+            char x = 'h';
+            char y = 'y';
+
+            foreach(char c in input)
+            {
+                str.Append(c);
+                if (c == 'e')
+                {
+                    str.Append(c);
+                }
+            }
+            Console.WriteLine(str.ToString());
+            
+        }
+    }
+}
